34e608de13a4e11432540a13ecee8da223a1e0e9
Your NameZahidul8 <www.zahidul8@gmail.com>
2025-08-27 12:21:42 +0600

    making the call button functionable

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ document.getElementById('card-container')
         getElement('counts-heart').innerText = heartCount;
 
     }
+   if (event.target.className.includes('btn-call')) {
+    const button = event.target;    
+    const serviceSubName = button.parentNode.parentNode.children[2].innerText;
+    const serviceNumber = button.parentNode.parentNode.children[3].innerText;
+    let availableCoin = parseInt(getElement('available-coin').innerText);
+    
+    if (availableCoin < 20) {
+        alert('❌ “You don’t have enough coins. You need at least 20 coins to make a call.”')
+        return;
+    }
+    alert(`📞Calling ${serviceSubName} ${serviceNumber}...`);
+    availableCoin -= 20;
+    getElement('available-coin').innerText = availableCoin;
    
+    
+    
+   }
     
 })
